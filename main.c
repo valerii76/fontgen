ea534ce6f43a2a897d76d5bad3dd6677171cbea2
Valery Volgutov <valery.volgutov@lge.com>
2012-06-28 09:40:52 +0400

    first init

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/28/2012 09:40:00 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Valery Volgutov (Valery Volgutov), valery.volgutov@lge.com
+ *        Company:  LGE
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    printf ("fontgen\n");
+    return 0;
+}
