641598f7f6eff38fee1e3c700fc64ebfb2c037c4
Valery Volgutov <valery.volgutov@lge.com>
2012-07-04 18:35:38 +0400

    add code for generate bit mask

--- main.c
+++ main.c
@@ -24,6 +24,17 @@
 
 #include <freetype/ftglyph.h>
 
+void fontgen_log (char const *format, ...)
+{
+    va_list args;
+    va_start (args, format);
+
+    vprintf (format, args);
+    printf ("\n");
+
+    va_end (args);
+}
+
 #define TO_STR(x) #x
 
 #define CHECK_FT(exp) \
@@ -63,8 +74,10 @@ int main (int argc, char *argv[])
     CHECK_FT (FT_Set_Char_Size (g_face, 0, font_size << 6, 300, 300));
     CHECK_FT (FT_Set_Pixel_Sizes (g_face, 0, font_size));
 
+    fontgen_log ("unsigned char font_%s_%d = {",
+            g_face->family_name, font_size);
     {
-        int i, j;
+        int i, j, k;
         FT_Bitmap *bitmap = NULL;
         FT_BitmapGlyph bm_glyph;
         FT_Glyph glyph;
@@ -72,6 +85,7 @@ int main (int argc, char *argv[])
 
         for (i = 0; i < strlen (font_sample); ++i)
         {
+            int h;
             index = FT_Get_Char_Index (g_face, font_sample[i]);
             CHECK_FT (FT_Load_Glyph (g_face, index, FT_LOAD_TARGET_MONO));
             CHECK_FT (FT_Get_Glyph (g_face->glyph, &glyph));
@@ -80,36 +94,53 @@ int main (int argc, char *argv[])
             bm_glyph = (FT_BitmapGlyph)glyph;
             bitmap = &bm_glyph->bitmap;
 
-            printf ("glyph (%c): %d, %d, %d, %d, %d\n"
-                    , font_sample[i], bitmap->rows, bitmap->width
-                    , bitmap->pitch, bitmap->num_grays
-                    , bitmap->pixel_mode);
+            /*printf ("glyph (%c): %d, %d, %d, %d, %d\n"*/
+                    /*, font_sample[i], bitmap->rows, bitmap->width*/
+                    /*, bitmap->pitch, bitmap->num_grays*/
+                    /*, bitmap->pixel_mode);*/
 
-            for (j = 0; j < bitmap->rows; ++j)
+            h = (bitmap->rows >> 3) + (bitmap->rows & 0x7 ? 1 : 0);
+
+            printf ("    '%c', 0x%x, ", font_sample[i], ((h << 5) | (bitmap->width & 0x1F)));
+
+            for (k = 0; k < bitmap->width; ++k)
             {
-                int w = 0;
-                int wb = bitmap->width / 8 + bitmap->width & 0x7 ? 1 : 0;
-                for (w = 0; w <= wb; ++w)
+                unsigned char b = 0;
+                for (j = 0; j < bitmap->rows; ++j)
                 {
-                    int k = 8;
-                    int b = bitmap->buffer[j * bitmap->pitch + w];
-                    while (k--)
-                    {
-                        if ((b & 0x80))
-                            printf ("*");
-                        else
-                            printf (" ");
-                        b <<= 1;
-                    }
+                    int idx = k & 0x07;
+                    unsigned char buf = bitmap->buffer[j * bitmap->pitch + (k >> 3)];
+                    b |= (buf & (1 << idx) >> idx) << (j & 0x7);
                 }
-                printf ("\n");
+
+                printf ("0x%x, ", b);
             }
+            printf ("\n");
 
-
+            /*for (j = 0; j < bitmap->rows; ++j)*/
+            /*{*/
+                /*int w = 0;*/
+                /*int wb = bitmap->width / 8 + bitmap->width & 0x7 ? 1 : 0;*/
+                /*for (w = 0; w <= wb; ++w)*/
+                /*{*/
+                    /*int k = 8;*/
+                    /*int b = bitmap->buffer[j * bitmap->pitch + w];*/
+                    /*while (k--)*/
+                    /*{*/
+                        /*if ((b & 0x80))*/
+                            /*printf ("*");*/
+                        /*else*/
+                            /*printf (" ");*/
+                        /*b <<= 1;*/
+                    /*}*/
+                /*}*/
+                /*printf ("\n");*/
+            /*}*/
 
             FT_Done_Glyph (glyph);
         }
     }
+    fontgen_log ("};\n");
 
     CHECK_FT (FT_Done_Face (g_face));
     CHECK_FT (FT_Done_FreeType (g_library));
